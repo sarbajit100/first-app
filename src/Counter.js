cef6b71cbe88b551c7ee3d322c2154a3c0a9c3ff
JACKS9969 <101617802+JACKS9969@users.noreply.github.com>
2023-07-24 17:18:23 +0530

    new

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,38 @@
 import {useState} from 'react';
-
+    // let z = 10;
 function Counter() {
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
-    return(
-        <>
-        Count: {x}
+    
+    // const [x, setX] = useState(0);
+    // const [y, setY] = useState(0);
+
+    // const [isEditting, setIseditting] = useState(false);
+    // const [todos, setTodos] = useState(['todo 1', 'todo 2']);
+    // return(
+    //     <>
+    //     <ul>
+    //     {todos.map((todo) => <li>{todo}</li>)}
+    //     <button onClick={()=> setTodos([... todos, 'Another one'])}>Click</button>
+    //     </ul>
+        
+
+        {/* {
+        (isEditting) ? <input /> : <p>some todo</p>
+        }
+        <br />
+        <button onClick={() => setIseditting(!isEditting)}>Click</button> */}
+
+        {/* count Z: {z}
+        <button onClick={() => z+=1}>Inc</button>
+        <br /> */}
+        
+        {/* Count: {x} is {(x % 2 == 0)? 'Even' : 'odd'}
         <button onClick={() => setX(x+1)}>Inc</button>
         <button onClick={() => setX(x-1)}>Dec</button>
-        <br />
-        Count: {y}
+        <br /> */}
+
+        {/* Count: {y}
         <button onClick={() => setY(y+1)}>Inc</button>
-        <button onClick={() => setY(y-1)}>Dec</button>
+        <button onClick={() => setY(y-1)}>Dec</button> */}
         </>
     )
 }
---
+++ src/avatar.jsx
@@ -0,0 +1,9 @@
+function avatar(props) {
+    return(
+        <>
+        <img src={props.src} width={props.width} height={props.height} />
+        </>
+    )
+}
+
+export default avatar;
